b2ce27d46e2b65c087ec729fce4e548b646a1c54
Marylyn Uzoukwu <muzoukwu@terpmail.umd.edu>
2024-04-16 19:32:29 -0400

    I edited the date_weather function

--- Final Project.py
+++ Final Project.py
@@ -12,13 +12,19 @@ df_filepath = Path(__file__).parent / "WeatherDataSet.csv"
 
 def date_weather(filepath):
     """
-    Deterines the weather for a date in April 2024, based on the user inputted date.
+    Determines the weather for a date in April 2024, based on the user inputted date.
         Args: 
             stri filepath to the csv file
+
+        Raises:
+            ValueError: User inputted date does not match the M/D/YYYY format 
+        
+        Raises:
+            ValueError: User inputed date is not in the CSV dataframe
+
         Returns:
             string of the weather correpsonding to the date in the csv file
     """
-    #add doc string for the fucntion 
     user_input = input("Input a date from April 2024 as M/D/YYYY, ie. 4/1/2024")
     try:
         date_pattern = r"(?P<month>\d{1})(?P<slash1>\/)(?P<day>\d{1,2})(?P<slash2>\/)(?P<year>\d{4})"
@@ -27,26 +33,23 @@ def date_weather(filepath):
     except:
         raise ValueError("Format is incorrect. Ensure you are using the M/D/YYYY")
 
-     # check to make sure that date is in the range, use logicical epxresions if, else etc.
-    month = int(match.group("day"))
+    
+    month = int(match.group("month"))
     day = int(match.group("day"))
     year =int(match.group("year"))
 
     if month != 4 or day < 1 or day > 30 or year != 2024:
-        raise ValueError ("Ensure that the date entred as a date in April 2024")
+        raise ValueError ("Ensure that the date entered as a date in April 2024")
     
 
     df = pd.read_csv(filepath) #loads the dataframe
-    #date_column = df.loc[:"Date"] #acesses the data in the date column
     
-    df_date = df.loc[df["Date"]==user_input] #filers just the row of data where the user_input = date
+    df_date = df.loc[df["Date"]==user_input] #filters just the row of data where the user_input = date
     if len(df_date) > 0:
-        weather = df_date.at[0,"Weather"]
+        weather = df_date.iloc[0]["Weather"]
         return weather
-        #select the vlaue at 1st row & the 2nd colunm 
-        #save that vlaue to weather variable
     else:
-        print("Check the format of your date") #should this be a return or try/except?
+        print("Check the format of your date") 
     
     
 
